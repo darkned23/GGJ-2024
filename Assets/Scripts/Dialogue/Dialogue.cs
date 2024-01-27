c33e59910ed50f95920e157e32cf9129219e711a
darkned <darkned.23@gmail.com>
2024-01-27 16:56:04 -0500

    Sistema de dialogo

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class Dialogue : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class Dialogue : MonoBehaviour
     [SerializeField] private TMP_Text dialogueText;
     [SerializeField] private GameObject dialogueMark;
     [SerializeField, TextArea(4, 6)] private String[] dialogueLines;
+    [SerializeField] private float typingTime = 0.05f;
     private bool isPlayerInRange;
     private bool didDialogueStart;
     private int lineIndex;
@@ -20,6 +22,15 @@ public class Dialogue : MonoBehaviour
             {
                 StartDialogue();
             }
+            else if (dialogueText.text == dialogueLines[lineIndex])
+            {
+                NextDialogueLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[lineIndex];
+            }
         }
     }
     private void StartDialogue()
@@ -28,6 +39,36 @@ public class Dialogue : MonoBehaviour
         dialoguePanel.SetActive(true);
         dialogueMark.SetActive(false);
         lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine("ShowLine");
+    }
+    private void NextDialogueLine()
+    {
+        lineIndex++;
+
+        if (lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine("ShowLine");
+        }
+        else
+        {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            dialogueMark.SetActive(true);
+            Time.timeScale = 1f;
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
+
+
     }
     private void OnTriggerEnter(Collider other)
     {
