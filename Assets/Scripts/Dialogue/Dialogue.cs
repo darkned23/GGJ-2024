9a5cb6f8740c6ccba6fd13493612d0ed9282bf35
darkned <darkned.23@gmail.com>
2024-01-31 08:01:25 -0500

    Cambio de idioma parte 01

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,7 +1,8 @@
+using TMPro;
 using System;
 using UnityEngine;
-using TMPro;
 using System.Collections;
+using UnityEngine.Localization.Settings;
 
 public class Dialogue : MonoBehaviour
 {
@@ -9,15 +10,19 @@ public class Dialogue : MonoBehaviour
     public GameObject dialoguePanel;
     public TMP_Text dialogueText;
     public GameObject dialogueMark;
-    [TextArea(4, 6)] public String[] dialogueLines;
+
+    [Header("Config Table")]
+    public string tableName;
+    public String[] dialogueKeys;
     public float typingTime = 0.05f;
+
     [Header("Other config")]
     [SerializeField] private bool canBeGrabbed;
 
     private bool isPlayerInRange;
     private bool didDialogueStart;
     private int lineIndex;
-
+    private string currentLine;
     private void Update()
     {
         if (isPlayerInRange && (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.JoystickButton3)))
@@ -26,14 +31,14 @@ public class Dialogue : MonoBehaviour
             {
                 StartDialogue();
             }
-            else if (dialogueText.text == dialogueLines[lineIndex])
+            else if (dialogueText.text == currentLine)
             {
                 NextDialogueLine();
             }
             else
             {
                 StopAllCoroutines();
-                dialogueText.text = dialogueLines[lineIndex];
+                dialogueText.text = currentLine;
             }
         }
     }
@@ -44,13 +49,18 @@ public class Dialogue : MonoBehaviour
         dialogueMark.SetActive(false);
         lineIndex = 0;
         Time.timeScale = 0f;
+
+        // Obtén la traducción desde la Localization Table
+        LocalizationSettings.StringDatabase.DefaultTable = tableName;
+        dialogueText.text = LocalizationSettings.StringDatabase.GetLocalizedString(dialogueKeys[lineIndex]);
+
         StartCoroutine("ShowLine");
     }
     public void NextDialogueLine()
     {
         lineIndex++;
 
-        if (lineIndex < dialogueLines.Length)
+        if (lineIndex < dialogueKeys.Length)
         {
             StartCoroutine("ShowLine");
         }
@@ -69,15 +79,18 @@ public class Dialogue : MonoBehaviour
 
     public IEnumerator ShowLine()
     {
+        // Utiliza la clave para obtener la traducción
+        currentLine = LocalizationSettings.StringDatabase.GetLocalizedString(dialogueKeys[lineIndex]);
+
+        Debug.Log("Current Line Content: " + currentLine);
         dialogueText.text = string.Empty;
-        foreach (char ch in dialogueLines[lineIndex])
+        foreach (char ch in currentLine)
         {
             dialogueText.text += ch;
             yield return new WaitForSecondsRealtime(typingTime);
         }
-
-
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
